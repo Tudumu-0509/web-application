dd6ea39740d98ecfdf8a92a4f3fb2bc1dc9800a6
Tudumu-0509 <ganeshtudumu@gmail.com>
2025-11-16 00:02:35 +0530

    Update backend.tf

--- terraform-jenkins/backend.tf
+++ terraform-jenkins/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "ganeshtudumu-s3bucket-123458"
+    bucket = "ganeshtudumu-s3bucket-1234581"
     key    = "terraform-state/terraform.tfstate"
     region = "us-east-1"
   }
