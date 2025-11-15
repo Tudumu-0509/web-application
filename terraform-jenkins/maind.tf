2416b27f69a9940b43224d8d5e67df910163914e
Tudumu-0509 <ganeshtudumu@gmail.com>
2025-11-15 23:00:26 +0530

    Update maind.tf

--- terraform-jenkins/maind.tf
+++ terraform-jenkins/maind.tf
@@ -22,3 +22,24 @@ resource "aws_instance" "my_ec2" {
     Name = var.instance_name
   }
 }
+
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = var.bucket_name
+}
+
+resource "aws_s3_bucket_versioning" "versioning" {
+  bucket = aws_s3_bucket.my_bucket.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "public_access" {
+  bucket = aws_s3_bucket.my_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
