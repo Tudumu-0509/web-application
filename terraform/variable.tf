e468ee044b9a43499f1c3b5528c1bb2899aedbd9
Tudumu-0509 <ganeshtudumu@gmail.com>
2025-11-15 19:00:06 +0530

    Create variable.tf

---
+++ terraform/variable.tf
@@ -0,0 +1,10 @@
+variable "bucket_name" {
+  description = "ganeshtudumu1234"
+  type        = string
+}
+
+variable "aws_region" {
+  description = "AWS region for S3 bucket"
+  type        = string
+  default     = "us-east-1"
+}
