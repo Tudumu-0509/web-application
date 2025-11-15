955eb9752585a5b70f94aa7eab49d5a7b390ecb9
Tudumu-0509 <ganeshtudumu@gmail.com>
2025-11-15 20:18:04 +0530

    Update variable.tf

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -1,10 +1,11 @@
-variable "bucket_name" {
-  description = "Name of the S3 bucket"
-  type        = string
-}
+
 
 variable "aws_region" {
   description = "AWS region for S3 bucket"
   type        = string
   default     = "us-east-1"
+}
+variable "bucket_name" {
+  description = "Name of the S3 bucket"
+  type        = string
 }
