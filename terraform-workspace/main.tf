56ab1030cb36fdbd17df1741e86bba50d02677ff
Tudumu-0509 <ganeshtudumu@gmail.com>
2025-11-19 00:41:06 +0530

    Update main.tf

--- terraform-workspace/main.tf
+++ terraform-workspace/main.tf
@@ -1,1 +1,21 @@
+provider "aws" {
+  region = var.region
+}
+
+resource "aws_instance" "my_ec2" {
+  ami           = var.ami
+  instance_type = var.instance_type
+
+  tags = {
+    Name = "${var.env}-server"
+  }
+}
+
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "${var.env}-myapp-bucket-12345"
+
+  tags = {
+    Environment = var.env
+  }
+}
 
