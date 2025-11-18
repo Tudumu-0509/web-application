7fa1e5d51a36ae6ccc783de9ca917d29af32a7fc
Tudumu-0509 <ganeshtudumu@gmail.com>
2025-11-19 00:36:07 +0530

    Update backend.tf

--- terraform-workspace/backend.tf
+++ terraform-workspace/backend.tf
@@ -1,1 +1,10 @@
 
+terraform {
+  backend "s3" {
+    bucket         = "terraform-backend-ganesh-bucket-001"
+    key            = "ec2/terraform.tfstate"
+    region         = "ap-south-1"
+    dynamodb_table = "terraform-lock-table"
+    encrypt        = true
+  }
+}
