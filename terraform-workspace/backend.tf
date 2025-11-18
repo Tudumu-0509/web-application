7b1d8a3a6e1dd8a8e0a76a3760aa4dd5cae59e01
Tudumu-0509 <ganeshtudumu@gmail.com>
2025-11-19 01:14:03 +0530

    Update backend.tf

--- terraform-workspace/backend.tf
+++ terraform-workspace/backend.tf
@@ -1,9 +1,9 @@
 
 terraform {
   backend "s3" {
-    bucket = "terraform-backend-ganesh-bucket-001"
-    key    = "ec2/${terraform.workspace}/terraform.tfstate"
-    region = "ap-south-1"
+    bucket  = "terraform-backend-ganesh-bucket-001"
+    key     = "ec2/terraform.tfstate"
+    region  = "ap-south-1"
     encrypt = true
   }
 }
