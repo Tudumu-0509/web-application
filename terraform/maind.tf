a412560a7e398aa63369dcb7be6de2e49451856b
Tudumu-0509 <ganeshtudumu@gmail.com>
2025-11-19 16:49:21 +0530

    Update maind.tf

--- terraform/maind.tf
+++ terraform/maind.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "app" {
   ami           = var.ami_id
   instance_type = var.instance_type
+
   tags = {
     Name        = "${var.environment}-ec2"
     Environment = var.environment
@@ -10,9 +11,9 @@ resource "aws_instance" "app" {
 resource "aws_s3_bucket" "bucket" {
   bucket = "${var.bucket_name}-${var.environment}"
   acl    = "private"
+
   tags = {
     Name        = "${var.environment}-bucket"
     Environment = var.environment
   }
 }
-
