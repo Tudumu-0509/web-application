1991eedc026a009cc8418d2012956212b9ca4cb0
Tudumu-0509 <ganeshtudumu@gmail.com>
2025-11-19 16:47:51 +0530

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,18 +1,3 @@
 terraform {
-  backend "s3" {
-    bucket  = var.backend_bucket
-    key     = "terraform.tfstate"
-    region  = var.aws_region
-    encrypt = true
-  }
-}
-
-variable "backend_bucket" {
-  type        = string
-  description = "S3 bucket to store Terraform state"
-}
-
-variable "aws_region" {
-  type        = string
-  description = "AWS region for the S3 bucket"
+  backend "s3" {}
 }
