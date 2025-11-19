f55253df39cc62140390b59f0f320def62488b31
Tudumu-0509 <ganeshtudumu@gmail.com>
2025-11-19 15:51:36 +0530

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,1 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  # Enable workspace usage
+  backend "local" {}
+}
+
+provider "aws" {
+  region = var.aws_region
+}
 
