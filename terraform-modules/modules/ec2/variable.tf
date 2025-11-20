6625993ff256b22827165748a58b28293ff421cd
Tudumu-0509 <ganeshtudumu@gmail.com>
2025-11-20 15:11:22 +0530

    Update variable.tf

--- terraform-modules/modules/ec2/variable.tf
+++ terraform-modules/modules/ec2/variable.tf
@@ -1,23 +1,22 @@
 variable "ami_id" {
   description = "AMI ID for EC2 instance"
   type        = string
-  default     = "ami-087d1c9a513324697"
 }
 
 variable "instance_type" {
   description = "Instance type"
   type        = string
-  default     = "t3.small"
+ 
 }
 
 variable "key_name" {
   description = "Key pair name"
   type        = string
-  default     = "connect"
+ 
 }
 
 variable "instance_name" {
   description = "EC2 Instance name tag"
   type        = string
-  default     = "My-EC2-Server"
+  
 }
