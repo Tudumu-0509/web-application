b924fe2917d30be414925eeeeb44fc4fd31ea3e4
Tudumu-0509 <ganeshtudumu@gmail.com>
2025-11-20 16:20:03 +0530

    Update main.tf

--- terraform-modules/main.tf
+++ terraform-modules/main.tf
@@ -13,3 +13,11 @@ module "ec2_module" {
 output "public_ip" {
   value = module.ec2_module.ec2_public_ip
 }
+
+module "ec2_module" {
+  source        = "./modules/ec2"
+  ami_id        = "ami-087d1c9a513324697"
+  instance_type = "t3.small"
+  key_name      = "connect"
+  instance_name = "My-EC2-Module1"
+}
